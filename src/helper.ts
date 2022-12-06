f9642ed104e66fd0a3de80ffc91a0b7f65741d7e
Roman Ivanov <rivanov@megaputer.ru>
2022-12-06 13:57:33 +0300

    Fixed work widget BarChart

--- src/helper.ts
+++ src/helper.ts
@@ -20,3 +20,11 @@ export function joinAnd(conditions: TConditionNode[]): TConditionNode {
 export function joinOr(conditions: TConditionNode[]): TConditionNode {
   return joinConditions(TColumnOperation.co_OR, conditions);
 }
+
+const BASE_DATE = Date.UTC(1899, 11, 30);
+const S_PER_DAY = 24 * 3600;
+const MS_PER_DAY = S_PER_DAY * 1000;
+export function variantToDate(daysAfterBaseDate: number, dateOnly?: boolean): Date {
+  const ms = dateOnly ? MS_PER_DAY * Math.trunc(daysAfterBaseDate) : Math.round(MS_PER_DAY * daysAfterBaseDate);
+  return new Date(BASE_DATE + ms);
+}
